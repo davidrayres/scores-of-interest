be7334c786d957f120d95aa30eb23bbb7644a9a0
davidrayres <davidrayres@hotmail.com>
2022-11-29 16:27:05 -0500

    change from loading all games at once to loading dynamically by week

--- script/app.js
+++ script/app.js
@@ -1,3 +1,6 @@
+
+// ESPN ENDPOINTS ==============================================
+
 const EndPoints = {
   NFL: {
     teams: 'http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams',
@@ -16,14 +19,17 @@ const EndPoints = {
   },
 }
 
-//****************** ERROR CATCH??? **************** */
+// GET DATA ====================================================
 
 const getNFLTeams = async () => {
+  //********** ERROR CATCH??? **************** */
   const resp = await fetch(EndPoints.NFL.teams)
   const data = await resp.json()
   console.log('NFL ENDPOINT - Teams', data)
   return data.sports[0].leagues[0].teams
 }
+
+// -------------------------------------------------------------
 
 const getNflCalendar = async () => {
   const resp = await fetch(EndPoints.NFL.calendar)
@@ -31,6 +37,8 @@ const getNflCalendar = async () => {
   console.log('NFL ENDPOINT - Calendar', data)
   return data
 }
+
+// -------------------------------------------------------------
 
 const getNFLGames = async () => {
   const resp = await fetch(EndPoints.NFL.games)
@@ -68,116 +76,148 @@ const getNFLGames = async () => {
   })
 }
 
-const weekNav = () => {
-  NFLdata.calendar.sections.entries[1].forEach((week) => {
-    const weekNav = `<div class="week-nav">${week.value}</div>`
-    getElementById('WeeklyScoreNav').append(weekNav)
-  })
-}
+// STAGE COMPONENTS ============================================
+// =============================================================
+// =============================================================
 
-const loadNflGames = (calendarData, gameData) => {
-  const scores = document.getElementById('Scores')
+const loadWeekNav = (calendarData, weekNum) => {
+  const weekBtnContainer = document.getElementById('WeekBtnContainer')
 
-  // insert weeks...
   calendarData.sections[1].entries.forEach((week) => {
     const weekNav = document.createElement('div')
-    weekNav.setAttribute('class', 'week-nav')
+    weekNav.setAttribute('class', 'week-btn')
     weekNav.innerHTML = week.value
-    document.getElementById('WeeklyScoreNav').append(weekNav)
+    weekBtnContainer.append(weekNav)
+  })
 
-    const gameWeek = document.createElement('div')
-    gameWeek.setAttribute('id', `nflWeek${week.value}`)
-    const weekLabel = `<div class="game-week">${week.label}</div>`
-    gameWeek.innerHTML = weekLabel
+  weekBtnContainer.children.item(weekNum).classList.add('current')
+}
 
-    // insert days...
-    const gameDays = calendarData.eventDate.dates.filter(
-      (gameDay) => gameDay >= week.startDate && gameDay <= week.endDate
-    )
+// -------------------------------------------------------------
 
-    gameDays.forEach((day, index) => {
-      const gameDay = document.createElement('div')
-      gameDay.setAttribute('id', `nflDay${week.value}-${index}`)
+const loadNflGames = (calendarData, gameData, weekNum) => {
+  const selectedWeek = calendarData.sections[1].entries[weekNum]
+  const scoresContainer = document.getElementById('ScoresContainer')
+  const existingScores = scoresContainer.querySelectorAll('div:not(#WeekLabel)')
+  const weekLabel = document.getElementById('WeekLabel')
 
-      const dayLabel = `<div class="gameday-label">${dayjs(day)
-        .format('dddd, MMMM D, YYYY')
-        .toUpperCase()}</div>`
-      gameDay.innerHTML = dayLabel
+  // remove existing scores...
+  existingScores.forEach(x => x.remove())
 
-      const gamePods = document.createElement('div')
-      gamePods.setAttribute('id', `gamePods${week.value}-${index}`)
-      gamePods.setAttribute('class', 'game-pods')
+  //set week label...
+  weekLabel.innerHTML = `WEEK ${weekNum + 1}`
 
-      gameDay.append(gamePods)
-      gameWeek.append(gameDay)
+  //filter calendar data to current week...
+  const gameDays = calendarData.eventDate.dates.filter(
+    (date) => date >= selectedWeek.startDate && date <= selectedWeek.endDate
+  )
+  console.log(gameDays)
+  // construct containers for each day .............................
+  gameDays.forEach((day, index) => {
+    //construct game day container...
+    const gameDayContainer = document.createElement('div')
+    gameDayContainer.setAttribute('id', `nflDay${selectedWeek.value}-${index}`)
 
-      // insert games...
-      const games = gameData.filter((game) => {
-        return game.date.split('T')[0] === day.split('T')[0]
-      })
+    //construct date label and insert into gameday container...
+    const gameDayLabel = document.createElement('div')
+    gameDayLabel.setAttribute('class', 'date-label')
+    gameDayLabel.innerHTML = dayjs(day)
+      .format('dddd, MMMM D, YYYY')
+      .toUpperCase()
+    gameDayContainer.append(gameDayLabel)
 
-      games.forEach((game) => {
-        const gamePod = document.createElement('div')
-        gamePod.setAttribute('id', `${game.id}`)
-        gamePod.classList.add('game-pod')
+    //construct gamepod container and insert into gameday container...
+    const gamePodsContainer = document.createElement('div')
+    gamePodsContainer.setAttribute('id', `gamePodsContainer${index}`)
+    gamePodsContainer.setAttribute('class', 'game-pods')
 
-        const gameDetail = `
-          <div class="time">${game.time}</div>
-          <table>
-            <tr class="game-head">
-              <td class="rank"></td>
-              <td class="logo"></td>
-              <td class="name"></td>
-              <th class="score"></th>
-              <th class="spacer"></th>
-              <th class="spread">Line</th>
-              <th class="total">Total</th>
-            </tr>
-            <tr class="away">
-              <td class="rank">-</td>
-              <td class="logo"><img src="${game.awayTeamLogo}"></td>
-              <td class="name">${game.awayTeamName}</td>
-              <td class="score">${game.awayTeamScore}</td>
-              <td class="spacer"></td>
-              <td class="odds spread">${'+22.2'}</td>
-              <td class="odds total">o${'22.2'}</td>
-            </tr>
-            <tr class="home">
-              <td class="rank">-</td>
-              <td class="logo"><img src="${game.homeTeamLogo}"></td>
-              <td class="name">${game.homeTeamName}</td>
-              <td class="score">${game.homeTeamScore}</td>
-              <td class="spacer"></td>
-              <td class="odds spread">${'-##.#'}</td>
-              <td class="odds total">u${'##.#'}</td>
-            </tr>
-          </table>
-        `
-        gamePod.innerHTML = gameDetail
-        gamePods.append(gamePod)
-        gamePod.querySelector('.away').dataset.winner = game.awayTeamWin
-        gamePod.querySelector('.home').dataset.winner = game.homeTeamWin
-      })
+    // insert games...
+    // filter game data to current week...
+    // ########## figure out how to pick thursdays...given GMT timezone
+    const games = gameData.filter((game) => {
+      return game.date.split('T')[0] === day.split('T')[0]
     })
 
-    scores.append(gameWeek)
+    games.forEach((game) => {
+      //construct game pod...
+      const gamePod = document.createElement('div')
+      gamePod.setAttribute('id', `${game.id}`)
+      gamePod.classList.add('game-pod')
+
+      //construct game detail...
+      const gameDetail = `
+        <div class="time">${game.time}</div>
+        <table>
+          <tr class="game-head">
+            <td class="rank"></td>
+            <td class="logo"></td>
+            <td class="name"></td>
+            <th class="score"></th>
+            <th class="spacer"></th>
+            <th class="spread">Line</th>
+            <th class="total">Total</th>
+          </tr>
+          <tr class="away">
+            <td class="rank">-</td>
+            <td class="logo"><img src="${game.awayTeamLogo}"></td>
+            <td class="name">${game.awayTeamName}</td>
+            <td class="score">${game.awayTeamScore}</td>
+            <td class="spacer"></td>
+            <td class="odds spread">${'+22.2'}</td>
+            <td class="odds total">o${'22.2'}</td>
+          </tr>
+          <tr class="home">
+            <td class="rank">-</td>
+            <td class="logo"><img src="${game.homeTeamLogo}"></td>
+            <td class="name">${game.homeTeamName}</td>
+            <td class="score">${game.homeTeamScore}</td>
+            <td class="spacer"></td>
+            <td class="odds spread">${'-##.#'}</td>
+            <td class="odds total">u${'##.#'}</td>
+          </tr>
+        </table>
+      `
+
+      //insert each game detail into gamePod and each gamePod into gamepod container...
+      gamePod.innerHTML = gameDetail
+      gamePod.querySelector('.away').dataset.winner = game.awayTeamWin
+      gamePod.querySelector('.home').dataset.winner = game.homeTeamWin
+      gamePodsContainer.append(gamePod)
+    })
+
+    //insert gamepod container into gameday container, then insert gameday container into scores container...
+    gameDayContainer.append(gamePodsContainer)
+    scoresContainer.append(gameDayContainer)
   })
 }
+
+// CONSTRUCT PAGE ==============================================
+// =============================================================
+// =============================================================
 
 const constructScores = async () => {
   const NFLdata = {}
+  let weekNum = 0
+
   NFLdata.teams = await getNFLTeams()
   NFLdata.calendar = await getNflCalendar()
   NFLdata.games = await getNFLGames()
   console.log('NFLdata', NFLdata)
 
-  loadNflGames(NFLdata.calendar, NFLdata.games)
+  loadWeekNav(NFLdata.calendar, weekNum)
+  loadNflGames(NFLdata.calendar, NFLdata.games, weekNum)
 
-  const weekButtons = document.querySelectorAll('.week-nav')
+  const weekButtons = document.querySelectorAll('.week-btn')
+
   weekButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
-      document.querySelector('.current').classList.remove('current')
+      weekNum = parseInt(e.target.innerText) - 1
+      document
+        .querySelector('#WeekBtnContainer .current')
+        .classList.remove('current')
       button.classList.add('current')
+
+      loadNflGames(NFLdata.calendar, NFLdata.games, weekNum)
     })
   })
 }
